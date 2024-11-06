d9d5960be0014fa9e45684d7898b42a509254e0c
Rohit Kumawat <rohitkumavatgame@gmail.com>
2024-11-06 15:07:18 +0530

    Basic PDF genration with logo and text

--- from fpdf import FPDF.py
+++ from fpdf import FPDF.py
@@ -1,1 +1,16 @@
 from fpdf import FPDF
+
+pdf=FPDF(orientation='P',unit='pt',format='A4') #pdf basic formate
+
+#adding pgae in pdf
+pdf.add_page()
+
+#adding image in pdf
+pdf.image('logo.png',w=50,h=50,x=300)
+pdf.set_font(family='arial',size=14)
+pdf.cell(w=0,h=50,txt='Valorent which is also know as Valo is 5vs 5 FPS game.')
+
+#saving pdf and nameing
+pdf.output('pdf01.pdf')
+
+
